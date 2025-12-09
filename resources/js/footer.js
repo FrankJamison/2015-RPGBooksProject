a58d1c2faa86829f36801db890606059c494dee8
FrankJamison <frank@frankjamison.com>
2025-12-09 08:18:49 -0800

    first commit

---
+++ resources/js/footer.js
@@ -0,0 +1,12 @@
+// Start JavaScript Code
+
+/* Write Footer Section to Web Page */
+document.write('<div id="footer"><p>');
+document.write('&copy;Copyright 2015 All rights reserved. &middot; ');
+document.write('<a href="http://www.frankjamison.com">Frank Jamison</a><br />');
+document.write('Design by ');
+document.write('<a href="http://www.jamisonwebdesign.com/" title="Jamison Web Design">Jamison Web Design</a> &amp; ');
+document.write('<a href="http://www.kendahlin.com/" title="Ken Dahlin">Ken Dahlin</a>.');
+document.write('</p></div>');
+
+// End JavaScript Code
